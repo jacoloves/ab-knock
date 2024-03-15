117fe7f2968e31312e57ea1d2662a5deffe9ee29
shooonng <5511068t@gmail.com>
2024-03-16 03:29:59 +0900

    20240316

---
+++ rust/src/main.rs
@@ -0,0 +1,54 @@
+#[allow(unused_imports)]
+use proconio::input;
+
+/* ↓aoj */
+#[allow(unused_imports)]
+use std::cmp::Ordering;
+#[allow(unused_imports)]
+use std::collections::HashMap;
+#[allow(unused_imports)]
+use std::collections::HashSet;
+
+#[allow(unused_imports)]
+use std::{isize, usize};
+
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::collections::VecDeque;
+#[allow(unused_imports)]
+use std::io::*;
+#[allow(unused_imports)]
+use std::str::*;
+
+use std::fmt::Debug;
+use std::str::FromStr;
+
+use regex::Regex;
+
+#[allow(dead_code)]
+fn read_line<T: std::str::FromStr>() -> Vec<T>
+where
+    <T as FromStr>::Err: Debug,
+{
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    s.split_whitespace().map(|c| c.parse().unwrap()).collect()
+}
+/* ↑AOJ */
+
+fn main() {
+    abc344_a();
+}
+
+#[allow(dead_code)]
+fn abc344_a() {
+    input! {
+        s: String,
+    }
+
+    let re = Regex::new(r"\|[^|]*\|").unwrap();
+    let trans_s: String = re.replace_all(&s, "").to_string();
+
+    println!("{}", trans_s);
+}
