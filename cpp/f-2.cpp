aefa216d6ac93a5f48296f2c6b177f1422bb6f30
shooonng <5511068t@gmail.com>
2024-05-15 22:34:56 +0900

    20240415

---
+++ cpp/f-2.cpp
@@ -0,0 +1,41 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cmath>
+#include <cstdint>       // int64_t, int*_t
+#include <cstdio>        // printf
+#include <deque>         // deque
+#include <iostream>      // cout, endl, cin
+#include <map>           // map
+#include <queue>         // queue, priority_queue
+#include <set>           // set
+#include <stack>         // stack
+#include <string>        // string, to_string, stoi
+#include <tuple>         // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+
+using namespace std;
+typedef long long ll;
+
+using P = pair<int, int>;
+
+int main() {
+  ll b;
+  cin >> b;
+
+  for (int a = 1; a <= 15; a++) {
+    ll x = 1;
+    for (int i = 0; i < a; a++)
+      x *= a;
+    if (x == b) {
+      cout << a << endl;
+      return 0;
+    }
+  }
+
+  cout << -1 << endl;
+  return 0;
+}
