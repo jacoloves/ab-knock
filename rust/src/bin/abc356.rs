64d1cc04c016dacbeb6b5fece6f99bb375191bb4
shooonng <5511068t@gmail.com>
2024-06-03 09:07:15 +0900

    20240603

---
+++ rust/src/bin/abc356.rs
@@ -0,0 +1,81 @@
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::input;
+use proconio::marker::Usize1;
+
+/* ↓aoj */
+#[allow(unused_imports)]
+use std::cmp::Ordering;
+#[allow(unused_imports)]
+use std::collections::HashMap;
+#[allow(unused_imports)]
+use std::collections::HashSet;
+
+#[allow(unused_imports)]
+use std::vec;
+#[allow(unused_imports)]
+use std::{isize, usize};
+
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::collections::VecDeque;
+#[allow(unused_imports)]
+use std::io::*;
+#[allow(unused_imports)]
+use std::str::*;
+
+use std::fmt::Debug;
+use std::str::FromStr;
+
+#[allow(unused_imports)]
+use regex::Regex;
+
+#[allow(dead_code)]
+fn read_line<T: std::str::FromStr>() -> Vec<T>
+where
+    <T as FromStr>::Err: Debug,
+{
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    s.split_whitespace().map(|c| c.parse().unwrap()).collect()
+}
+/* ↑AOJ */
+
+fn main() {
+    c();
+}
+
+fn c() {
+    input! {
+        n: usize,
+        m: usize,
+        k: usize,
+    }
+
+    let ks = (0..m)
+        .into_iter()
+        .map(|_| {
+            input! {
+                c: usize,
+                a: [Usize1; c],
+                r: char
+            }
+            (a, r == 'o')
+        })
+        .collect_vec();
+
+    let mut res = 0;
+
+    'i: for i in 0..1 << n {
+        for (v, r) in &ks {
+            let num = v.iter().filter(|&&x| i >> x & 1 == 1).count();
+            if !((num >= k && *r) || (num < k && !*r)) {
+                continue 'i;
+            }
+        }
+        res += 1;
+    }
+
+    println!("{}", res);
+}
