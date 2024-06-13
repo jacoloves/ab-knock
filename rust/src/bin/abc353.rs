c573b4071950676691b28e240ef27a22fe1db941
shooonng <5511068t@gmail.com>
2024-06-14 07:32:38 +0900

    abc353 commit

---
+++ rust/src/bin/abc353.rs
@@ -0,0 +1,49 @@
+#[allow(unused_imports)]
+use proconio::input;
+
+/* ↓aoj */
+#[allow(unused_imports)]
+use std::cmp::Ordering;
+#[allow(unused_imports)]
+use std::collections::HashMap;
+#[allow(unused_imports)]
+use std::collections::HashSet;
+
+#[allow(unused_imports)]
+use std::vec;
+#[allow(unused_imports)]
+use std::{isize, usize};
+
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::collections::VecDeque;
+#[allow(unused_imports)]
+use std::io::*;
+#[allow(unused_imports)]
+use std::str::*;
+
+use std::fmt::Debug;
+use std::str::FromStr;
+
+use regex::Regex;
+
+fn main() {
+    a()
+}
+
+fn a() {
+    input! {
+        n: usize,
+        v: [usize; n],
+    }
+
+    for i in 1..n {
+        if v[0] < v[i] {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+
+    println!("-1");
+}
