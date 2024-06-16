0be7ad6f0e5183fa5da1d4087ba18dab9c54bc3a
shooonng <5511068t@gmail.com>
2024-06-16 22:01:55 +0900

    abc358 commit

---
+++ rust/src/bin/abc358.rs
@@ -0,0 +1,46 @@
+#[allow(unused_imports)]
+use proconio::input;
+
+/* ↓aoj */
+#[allow(unused_imports)]
+use std::cmp::Ordering;
+#[allow(unused_imports)]
+use std::collections::HashMap;
+#[allow(unused_imports)]
+use std::collections::HashSet;
+
+#[allow(unused_imports)]
+use std::vec;
+#[allow(unused_imports)]
+use std::{isize, usize};
+
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::collections::VecDeque;
+#[allow(unused_imports)]
+use std::io::*;
+#[allow(unused_imports)]
+use std::str::*;
+
+use std::fmt::Debug;
+use std::str::FromStr;
+
+use regex::Regex;
+
+fn main() {
+    a();
+}
+
+fn a() {
+    input! {
+        s: String,
+        t: String,
+    }
+
+    if s == "AtCoder" && t == "Land" {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
