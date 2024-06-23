60b29b87bcec2d0a9bc81364dc51c01b1ef5545d
shooonng <5511068t@gmail.com>
2024-06-23 21:04:23 +0900

    abc359 commit

---
+++ rust/src/bin/abc359.rs
@@ -0,0 +1,51 @@
+#[allow(unused_imports)]
+use proconio::input;
+
+/* ↓aoj */
+#[allow(unused_imports)]
+use std::cmp::Ordering;
+#[allow(unused_imports)]
+use std::collections::HashMap;
+#[allow(unused_imports)]
+use std::collections::HashSet;
+
+#[allow(unused_imports)]
+use std::vec;
+#[allow(unused_imports)]
+use std::{isize, usize};
+
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::collections::VecDeque;
+#[allow(unused_imports)]
+use std::io::*;
+#[allow(unused_imports)]
+use std::str::*;
+
+use std::fmt::Debug;
+use std::str::FromStr;
+
+use regex::Regex;
+
+fn main() {
+    a();
+}
+
+fn a() {
+    input! {
+        n: usize,
+        sv: [String; n],
+    }
+
+    let target = "Takahashi".to_string();
+
+    let mut cnt = 0;
+    for e in sv {
+        if e == target {
+            cnt += 1;
+        }
+    }
+
+    println!("{cnt}");
+}
